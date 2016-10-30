19668cb4cf5e7540401edba5b5753569dee0b2c7
Ayachi Gin <ayachigin@gmail.com>
2016-10-30 19:49:18 +0900

    Death in GlobalFree(but its works)

--- HsSaori.hs
+++ HsSaori.hs
@@ -2,35 +2,36 @@
 module HsSaori where
 
 import Foreign.C.String
-import Foreign.C.Types (CLong)
+import Foreign.C.Types (CInt(..))
 import Foreign.Marshal.Alloc
 import Foreign.Ptr (nullPtr, Ptr, castPtr)
 import System.Win32.Mem
 
-foreign export ccall load :: HGLOBAL -> Ptr CLong -> IO Bool
+foreign export ccall load :: HGLOBAL -> CInt -> IO Bool
 foreign export ccall unload :: IO Bool
-foreign export ccall request :: HGLOBAL -> Ptr CLong -> IO HGLOBAL
+foreign export ccall request :: HGLOBAL -> Ptr CInt -> IO HGLOBAL
 
-request :: HGLOBAL -> Ptr CLong -> IO HGLOBAL
+request :: HGLOBAL -> Ptr CInt -> IO HGLOBAL
 request h _ = do
   s <- toHsString $ castPtr h
-  writeFile "/input" s
+  writeFile "input" s
   (cstr, n) <- newCStringLen message
   let m = succ n -- '\0'
-  _ <- globalFree h
+  --_ <- globalFree h
   nh <- globalAlloc gMEM_FIXED (fromIntegral m)
   copyMemory nh (castPtr cstr) (fromIntegral m)
   free cstr
   return nh
   where
     message = "SAORI/1.0 200 OK\r\n" ++
-              "Charset: UTF-8\r\n\r\n"
+              "Charset: UTF-8\r\n" ++
+              "Result: fuga\r\n\r\n"
 
-load :: HGLOBAL -> Ptr CLong -> IO Bool
+load :: HGLOBAL -> CInt -> IO Bool
 load h _ = do
   cstr <- toHsString $ castPtr h
   writeFile (cstr ++ "hsdll") cstr
-  _ <- globalFree h
+  --_ <- globalFree h
   return True
 
 unload :: IO Bool
